d7d003812c09fcc785f62fd79a212a51a02ad21f
Daytonsheed01 <108303595+Daytonsheed01@users.noreply.github.com>
2024-07-26 05:11:54 +0100

    Add files via upload

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Samuel Abraham </span>
+            from <span className="purple"> Capte Town, South Africa.</span>
+            <br />
+            With over 4 years of experience in software and web development.
+            <br />
+            I've been transforming ideas into reality through code.
+            <br />
+            I specialize in building robust applications and responsive websites.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Writing Tech Blogs
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">SammyDcreator</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
