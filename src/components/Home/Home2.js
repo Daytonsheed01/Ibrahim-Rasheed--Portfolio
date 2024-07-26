787e60f4a28f4c5aa561590369cd2b83a141ae2b
Daytonsheed01 <108303595+Daytonsheed01@users.noreply.github.com>
2024-07-26 18:11:46 +0100

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -97,7 +97,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/daytonsheed"
+                  href="https://www.instagram.com/sammydcreator"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
